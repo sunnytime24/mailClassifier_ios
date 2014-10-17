4e3f6b0f7d7bce84a8485b2295daac90cdbd53de
Youngsun_Park <ys@Youngsun-Parkui-MacBook-Air.local>
2014-10-17 14:59:22 +0900

    Init Test !!!

--- mailClassifier/AppDelegate.m
+++ mailClassifier/AppDelegate.m
@@ -12,6 +12,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    NSLog(@"Application did finish launching");
     // Override point for customization after application launch.
     return YES;
 }
