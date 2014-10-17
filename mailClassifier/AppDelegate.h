0f5940ce15e549b20086734bde8b5816f5958cc4
Youngsun_Park <ys@Youngsun-Parkui-MacBook-Air.local>
2014-10-17 14:56:44 +0900

    Initial Commit

---
+++ mailClassifier/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  mailClassifier
+//
+//  Created by Youngsun_Park on 2014. 10. 17..
+//  Copyright (c) 2014년 Youngsun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ mailClassifier/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  mailClassifier
+//
+//  Created by Youngsun_Park on 2014. 10. 17..
+//  Copyright (c) 2014년 Youngsun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
